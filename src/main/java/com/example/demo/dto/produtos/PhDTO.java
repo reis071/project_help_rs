be2675c680fe215f838983a8434020f173f26287
reis071 <reiscorreia071@gmail.com>
2025-01-17 15:58:48 -0300

    add phDTO

---
+++ src/main/java/com/example/demo/dto/produtos/PhDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto.produtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PhDTO {
+    private String tipo;
+    private int quantidade;
+}
